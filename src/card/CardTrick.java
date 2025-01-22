b93d861a7efc8d2c174cf1ec6c0044692bfb7bb1
DougMeadows <meadowsd@sheridancollege.ca>
2025-01-21 22:04:59 -0500

    master_test

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package card;
+//test
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
